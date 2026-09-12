ade56650d320b77435b189e41b2aac298e4a8863
Handika Saputra <handikasaputra484@gmail.com>
2026-09-12 13:40:24 +0700

    Menghitung Gaji Karyawan materi 01-08

--- Latihan Gabungan/Gabungan Materi 01-08/Menghitung_Gaji_Karyawan.js
+++ Latihan Gabungan/Gabungan Materi 01-08/Menghitung_Gaji_Karyawan.js
@@ -2,7 +2,7 @@
 
 function hitungGaji (nama_Karyawan, gajiPokok, tunjangan, lembur, bonus, potonganLain){
     let pajak = gajiPokok * 0.1;
-    let totalPendapatan = gajiPokok + tunjangan + lembur + bonus + potonganLain;
+    let totalPendapatan = gajiPokok + tunjangan + lembur + bonus;
     let gajiBersih = totalPendapatan - pajak - potonganLain;
 
     let status;
@@ -17,22 +17,22 @@ function hitungGaji (nama_Karyawan, gajiPokok, tunjangan, lembur, bonus, potonga
     }
 
     return{
-        nama: nama,
+        nama: nama_Karyawan,
         gajiBersih: gajiBersih,
         status: status
     }
 }
 
 let daftarKaryawan = [
-    {nama: "Ririn", gajiPokok: 6800000, tunjangan: 1000000, lembur: 500000, bonus: 1500000, potongan: 700000},
-    {nama: "Handika", gajiPokok: 7500000, tunjangan: 1200000, lembur: 600000, bonus: 1000000, potongan: 800000},
-    {nama: "Budi", gajiPokok: 5000000, tunjangan: 800000, lembur: 300000, bonus: 500000, potongan: 600000}
+    {nama_Karyawan: "Ririn", gajiPokok: 6800000, tunjangan: 1000000, lembur: 500000, bonus: 1500000, potongan: 700000},
+    {nama_Karyawan: "Handika", gajiPokok: 7500000, tunjangan: 1200000, lembur: 600000, bonus: 1000000, potongan: 800000},
+    {nama_Karyawan: "Budi", gajiPokok: 5000000, tunjangan: 800000, lembur: 300000, bonus: 500000, potongan: 600000}
 ];
 
 for(let i = 0; i < daftarKaryawan.length; i++){
     let karyawan = daftarKaryawan[i];
     let hasil = hitungGaji(
-        karyawan.nama,
+        karyawan.nama_Karyawan,
         karyawan.gajiPokok,
         karyawan.tunjangan,
         karyawan.lembur,
@@ -40,7 +40,7 @@ for(let i = 0; i < daftarKaryawan.length; i++){
         karyawan.potongan
     );
 
-    console.log(`Nama: ${hasil.nama}`);
+    console.log(`Nama: ${hasil.nama_Karyawan}`);
     console.log(`Gaji Bersih: Rp ${hasil.gajiBersih}`);
     console.log(`Status: ${hasil.status}`);
     console.log("---------------------------");
