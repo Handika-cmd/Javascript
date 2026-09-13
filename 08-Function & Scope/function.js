b0b1654f044c2f70ac3cc0c7cacb85b02bd48aa9
Handika Saputra <handikasaputra484@gmail.com>
2026-09-14 04:35:30 +0700

    Function

--- 08-Function & Scope/function.js
+++ 08-Function & Scope/function.js
@@ -1,30 +1,5 @@
 // === 1. FUNCTION DECLARATION (Cara Standar) ===
 
-// Ini adalah cara membuat fungsi dengan kata kunci 'function'
-function hitungGaji(namaKaryawan, gajiPokok){
-    let pajak = gajiPokok * 0.1;
-    let gajiBersih = gajiPokok - pajak;
-
-    return gajiBersih;
-}
-
-// Cara memakai mesinnya (Memanggil/Call Function):
-let gajiRirin = hitungGaji("Ririn", 6500000);
-let gajiHandika = hitungGaji("Handika", 6500000);
-
-console.log("Gaji Bersih Ririn Dwi Aryanti: Rp. ", gajiRirin);
-console.log("Haji Bersih Handika Saputra: Rp. ", gajiHandika);
-
-function perkenalan(nama){
-    return "Nama saya, " + nama + "!!";
-}
-perkenalan("Handika Saputra");
-console.log(perkenalan("Handika Saputra"));
-
-// Menghitung luas dengan arrow modern
-const luas = (panjang, lebar) => panjang * lebar;
-console.log(luas(5, 3));
-
 // ==============================================================
 // Function dengan Dua Parameter
 // 1. tambah
@@ -56,3 +31,13 @@ function bagiSisa(a, b){
     console.log(a % b);
 }
 bagiSisa(5, 3);
+
+// Nah, sekarang kita membuat function dengan return
+function tambah(a, b){
+    return a + b;
+}
+
+let hasil = tambah(20, 10);
+console.log(hasil);
+
+// Sekarang masuk bagian kedua yaitu scope. silahkan buka file yang bernama scope.
