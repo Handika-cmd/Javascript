724605ff62933fa2f0a2404d40c28bcfda92b6ee
Handika Saputra <handikasaputra484@gmail.com>
2026-09-14 04:20:30 +0700

    Function & Scope/

--- 08-Function & Scope/function.js
+++ 08-Function & Scope/function.js
@@ -24,3 +24,35 @@ console.log(perkenalan("Handika Saputra"));
 // Menghitung luas dengan arrow modern
 const luas = (panjang, lebar) => panjang * lebar;
 console.log(luas(5, 3));
+
+// ==============================================================
+// Function dengan Dua Parameter
+// 1. tambah
+function tambah(a, b){
+    console.log(a + b);
+}
+tambah(5, 3);
+
+// 2. kurang
+function kurang(a, b){
+    console.log(a - b);
+}
+kurang(5, 3);
+
+// 3. bagi
+function bagi(a, b){
+    console.log(a / b);
+}
+bagi(5, 3);
+
+// kali
+function kali(a, b){
+    console.log(a * b);
+}
+kali(5, 3);
+
+// bagi sisa
+function bagiSisa(a, b){
+    console.log(a % b);
+}
+bagiSisa(5, 3);
