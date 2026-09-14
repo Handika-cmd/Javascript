022d9942c67cc0856a26aea88d2769e9ac3542c5
Handika Saputra <handikasaputra484@gmail.com>
2026-09-14 16:49:45 +0700

    Function & Scope

--- 08-Function & Scope/latihan/latihan.js
+++ 08-Function & Scope/latihan/latihan.js
@@ -11,3 +11,13 @@ function perkenalan(nama, umur){
 perkenalan("Dika", 22);
 perkenalan("Ririn Dwi Aryanti", 20);
 perkenalan("Rahmat Ramadan ", 300);
+
+
+// Latihan 3 - Return
+function namaPengguna(nama, umur, hobi){
+    return "Nama saya " + nama + ", umur " + umur + ", dan hobi " + hobi;
+}
+
+let pengguna = namaPengguna("Handika Saputra", 22, "Belajar Programan");
+let pengguna1 = namaPengguna("Ririn Dwi Aryanti", 20, "Mam enak ama mam spageti")
+console.log(pengguna);
