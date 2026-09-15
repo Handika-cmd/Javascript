b59f1d71c669e88e68dce2b343d93f3b33de89f8
Handika Saputra <handikasaputra484@gmail.com>
2026-09-15 09:59:07 +0700

    Array & Basic Operations

---
+++ 09-Array & Basic Operations/latihan.js
@@ -0,0 +1,10 @@
+let mahasiswa = ["Handika Saputra", "Ririn Dwi Aryanti", "Rahmat ramadan"];
+
+mahasiswa[2] = "RAHMAT RAMADHAN";
+mahasiswa[0] = "HANDIKA SAPUTRA";
+mahasiswa[1] = "RIRIN DWI ARYANTI";
+
+console.log(mahasiswa[0]);
+console.log(mahasiswa[1]);
+console.log(mahasiswa[2]);
+console.log(mahasiswa.length);
