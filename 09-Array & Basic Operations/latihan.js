12f06053da3f00669aef575480c5a59c589e8d98
Handika Saputra <handikasaputra484@gmail.com>
2026-09-15 10:39:30 +0700

    Array & Operaations

--- 09-Array & Basic Operations/latihan.js
+++ 09-Array & Basic Operations/latihan.js
@@ -1,4 +1,4 @@
-let mahasiswa = ["Handika Saputra", "Ririn Dwi Aryanti", "Rahmat ramadan"];
+// let mahasiswa = ["Handika Saputra", "Ririn Dwi Aryanti", "Rahmat ramadan"];
 
 mahasiswa[2] = "RAHMAT RAMADHAN";
 mahasiswa[0] = "HANDIKA SAPUTRA";
@@ -7,4 +7,21 @@ mahasiswa[1] = "RIRIN DWI ARYANTI";
 console.log(mahasiswa[0]);
 console.log(mahasiswa[1]);
 console.log(mahasiswa[2]);
+// console.log(mahasiswa.length);
+
+// Menambahkan Data Array
+let mahasiswa = ["Handika Saputra", "Ririn Dwi Aryanti", "Rahmat ramadan"];
+
+mahasiswa[2] = "RAHMAT RAMADHAN";
+mahasiswa[0] = "HANDIKA SAPUTRA";
+mahasiswa[1] = "RIRIN DWI ARYANTI";
+
+mahasiswa.push("Andi", "Abdurrahman", "Wildan");
+
+console.log(mahasiswa[0]);
+console.log(mahasiswa[1]);
+console.log(mahasiswa[2]);
+console.log(mahasiswa[3]);
+console.log(mahasiswa[4]);
+console.log(mahasiswa[5]);
 console.log(mahasiswa.length);
