253f845c7b5629d15f404207d0370f5d585ad056
Handika Saputra <handikasaputra484@gmail.com>
2026-09-15 11:37:39 +0700

    Array & Operations

--- 09-Array & Basic Operations/latihan.js
+++ 09-Array & Basic Operations/latihan.js
@@ -1,15 +1,15 @@
 // let mahasiswa = ["Handika Saputra", "Ririn Dwi Aryanti", "Rahmat ramadan"];
 
-mahasiswa[2] = "RAHMAT RAMADHAN";
-mahasiswa[0] = "HANDIKA SAPUTRA";
-mahasiswa[1] = "RIRIN DWI ARYANTI";
+// mahasiswa[2] = "RAHMAT RAMADHAN";
+// mahasiswa[0] = "HANDIKA SAPUTRA";
+// mahasiswa[1] = "RIRIN DWI ARYANTI";
 
-console.log(mahasiswa[0]);
-console.log(mahasiswa[1]);
-console.log(mahasiswa[2]);
-// console.log(mahasiswa.length);
+// console.log(mahasiswa[0]);
+// console.log(mahasiswa[1]);
+// console.log(mahasiswa[2]);
+// // console.log(mahasiswa.length);
 
-// Menambahkan Data Array
+// // Menambahkan Data Array
 let mahasiswa = ["Handika Saputra", "Ririn Dwi Aryanti", "Rahmat ramadan"];
 
 mahasiswa[2] = "RAHMAT RAMADHAN";
@@ -25,3 +25,24 @@ console.log(mahasiswa[3]);
 console.log(mahasiswa[4]);
 console.log(mahasiswa[5]);
 console.log(mahasiswa.length);
+
+// =========================================================================
+// CHALLENGE 09: MEMBUAT DAFTAR BELANJAAN MENGGUNAKAN ARRAY.
+// =========================================================================
+let daftarBelanja = [];
+daftarBelanja.push("Beras");
+daftarBelanja.push("Minyak");
+daftarBelanja.push("Kacang");
+daftarBelanja.push("Sayur kol");
+daftarBelanja.push("Apel");
+daftarBelanja.push("Mangga")
+
+console.log("Daftar Belanja: ", daftarBelanja);
+console.log(daftarBelanja.length);
+
+// mode hapus data menggunakan pop, shift
+daftarBelanja.pop();
+daftarBelanja.shift();
+
+console.log("Daftar Belanja Setelah diPop-shift", daftarBelanja);
+console.log(daftarBelanja.length);
