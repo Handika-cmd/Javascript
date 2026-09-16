e7eee34e7cee8373ab8b75f362c9436aee74731c
Handika Saputra <handikasaputra484@gmail.com>
2026-09-17 03:45:14 +0700

    Objects & Properties

---
+++ 10-Objects & Properties/latihan.js
@@ -0,0 +1,13 @@
+let biodata = {
+    nama: "Handika",
+    tempatLahir: "Tempirai",
+    umur: 22,
+    identitas: "Mahasiswa"
+};
+
+console.log("Biodata: ", biodata);
+
+console.log(biodata.nama);
+console.log(biodata.tempatLahir);
+console.log(biodata.umur);
+console.log(biodata.identitas);
