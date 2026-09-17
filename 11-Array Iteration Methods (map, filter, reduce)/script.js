0e9ca65c6447411d7aa4bd1688764a558764b388
Handika Saputra <handikasaputra484@gmail.com>
2026-09-18 05:09:22 +0700

    Array Iteration Methods

--- 11-Array Iteration Methods (map, filter, reduce)/script.js
+++ 11-Array Iteration Methods (map, filter, reduce)/script.js
@@ -6,3 +6,39 @@ let hargaMahal = harga.filter(function(harga){
 });
 
 console.log(hargaMahal);
+
+// Latihan 1 filter data harga tinggi
+let hargaBarang =[25000, 30000, 45000, 55000, 65000, 70000];
+
+let hargaTinggi = hargaBarang.filter(function(hargaBarang){
+    return hargaBarang >= 55000;
+})
+console.log("Harga Tinggi: ", hargaTinggi);
+
+// Latihan 2 — Filter data harga rendah
+let hargabarang = [25000, 30000, 45000, 55000, 65000, 70000];
+
+let hargaRendah = hargabarang.filter(function(harga){
+    return harga < 45000;
+});
+
+console.log(`Harga rendah: Rp ${hargaRendah}`);
+
+// Latihan 3 daftar harga
+
+let daftarHarga = [
+    15000,
+    25000,
+    50000,
+    65000,
+    70000,
+    75000,
+    80000
+];
+
+let hargaRendah = daftarHarga.filter(function(harga){
+    return harga < 65000;
+});
+
+console.log(`Daftar Harga: ${daftarHarga}`);
+console.log(`Harga Rendah: ${hargaRendah}`);
