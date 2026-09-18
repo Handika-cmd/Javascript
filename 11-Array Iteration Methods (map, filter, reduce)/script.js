ee883eb87a51b269d77180b348ed0eda59b915c2
Handika Saputra <handikasaputra484@gmail.com>
2026-09-18 15:11:10 +0700

    Array Iteration

--- 11-Array Iteration Methods (map, filter, reduce)/script.js
+++ 11-Array Iteration Methods (map, filter, reduce)/script.js
@@ -82,3 +82,57 @@ let mahasiswaInformatika = mahasiswa.filter(function(item){
 console.log("Daftar Mahasiswa Tarbiyah: ", mahasiswaTarbiyah);
 console.log("Daftar Mahasiswa Teknik: ", mahasiswaTeknik);
 console.log("Nama Mahasiswa Teknik Informatika: ", mahasiswaInformatika);
+
+
+// =================================================================================================================
+// =================================================================================================================
+// Latihan mengambil data mahasiswa menggunakan filter
+// =================================================================================================================
+let mahasiswa = [
+    { nama: "Dika", jurusan: "PAI", semester: 3 },
+    { nama: "Ririn", jurusan: "PAI", semester: 2 },
+    { nama: "Rahmat", jurusan: "PAI", semester: 3 },
+    { nama: "Andi", jurusan: "Informatika", semester: 4 },
+    { nama: "Siti", jurusan: "Informatika", semester: 4 },
+    { nama: "Budi", jurusan: "Informatika", semester: 2 },
+    { nama: "Lina", jurusan: "Manajemen", semester: 5 },
+    { nama: "Ahmad", jurusan: "Manajemen", semester: 5 },
+    { nama: "Tono", jurusan: "Manajemen", semester: 3 },
+    { nama: "Wulan", jurusan: "Ekonomi", semester: 2 },
+    { nama: "Rafi", jurusan: "Ekonomi", semester: 2 },
+    { nama: "Nisa", jurusan: "Ekonomi", semester: 4 },
+    { nama: "Bayu", jurusan: "Hukum", semester: 6 },
+    { nama: "Dewi", jurusan: "Hukum", semester: 6 },
+    { nama: "Yoga", jurusan: "Hukum", semester: 4 },
+    { nama: "Maya", jurusan: "Kedokteran", semester: 1 },
+    { nama: "Rangga", jurusan: "Kedokteran", semester: 1 },
+    { nama: "Putri", jurusan: "Kedokteran", semester: 3 },
+    { nama: "Fajar", jurusan: "Teknik Sipil", semester: 2 },
+    { nama: "Intan", jurusan: "Teknik Sipil", semester: 4 }
+];
+
+let mahasiswaHukum = mahasiswa.filter(function(item){
+    return item.jurusan === "Hukum";
+});
+
+let mahasiswaPAI = mahasiswa.filter(function(item){
+    return item.jurusan === "PAI";
+})
+
+let mahasiswaManajemen = mahasiswa.filter(function(item){
+    return item.jurusan === "Manajemen";
+});
+
+let mahasiswaEkonomi = mahasiswa.filter(function(item){
+    return item.jurusan === "Ekonomi";
+});
+
+let mahasiswaInformatika = mahasiswa.filter(function(item){
+    return item.jurusan === "Informatika";
+});
+
+console.log("Mahasiswa Hukum: ", mahasiswaHukum);
+console.log("Mahasiswa PAI: ", mahasiswaPAI);
+console.log("Mahasiswa Manajemen: ", mahasiswaManajemen);
+console.log("Mahasiswa Ekonomi: ", mahasiswaEkonomi);
+console.log("Mahasiswa Informatika: ", mahasiswaInformatika);
