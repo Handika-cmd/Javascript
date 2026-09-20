e5bcde07af693f496bdc129322255ea028109c7f
Handika Saputra <handikasaputra484@gmail.com>
2026-09-20 12:07:59 +0700

    DOM Selection & Traversal

--- 12-DOM Selection & Traversal/QuerySelector/querySelector.js
+++ 12-DOM Selection & Traversal/QuerySelector/querySelector.js
@@ -6,3 +6,11 @@ console.log(judul.textContent);
 let container = document.querySelector("#container");
 console.log(container);
 console.log(container.textContent);
+
+let container1 = document.querySelectorAll(".card p");
+console.log(container1[0]);
+console.log(container1[0].textContent);
+console.log(container1[1]);
+console.log(container1[1].textContent);
+console.log(container1[2]);
+console.log(container1[2].textContent);
