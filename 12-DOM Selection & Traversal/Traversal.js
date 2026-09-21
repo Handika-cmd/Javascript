3f332e1189fce22e59a8e892b5dabf1f7a5fe4bd
Handika Saputra <handikasaputra484@gmail.com>
2026-09-21 16:08:24 +0700

    DOM Selection & Traversal

--- 12-DOM Selection & Traversal/Traversal.js
+++ 12-DOM Selection & Traversal/Traversal.js
@@ -15,4 +15,7 @@ console.log(card.children[0].textContent);
 console.log(card.children[1].textContent);
 console.log(card.children[2].textContent);
 const parent1 = card.parentElement;
-console.log(parent);
+console.log(parent1);
+
+produk.parentElement.style.backgroundColor = 'yellow';
+produk.parentElement.style.color = 'red';
