da979ebb5b27246ba6ac2fe0fa229fcbaa314662
Handika Saputra <handikasaputra484@gmail.com>
2026-09-23 05:31:52 +0700

    Event Handling (click, input, submit)

--- 14-Event Handling (click, input, submit)/script.js
+++ 14-Event Handling (click, input, submit)/script.js
@@ -1,5 +1,14 @@
-let tombol = document.querySelector("#tombol");
-tombol.addEventListener("click", function(){
+let tombol1 = document.querySelector("#tombol");
+tombol1.addEventListener("click", function(){
     console.log("tombol diklik!!!");
     alert("Tombol di klik");
+});
+
+
+// Latihan ke 2 gabungan
+const tombol = document.querySelector("#tombol");
+
+tombol.addEventListener("click", function(){
+    judul.textContent = "Tombol sudah diklik!!!";
+    judul.style.color = "green"
 });
