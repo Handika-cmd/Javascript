83cf765d1c8cfabe3a71617bb42284236cc8292b
Handika Saputra <handikasaputra484@gmail.com>
2026-09-22 08:19:11 +0700

    DOM Manipulation & Style Modification

--- 13-DOM Manipulation & Style Modification/script.js
+++ 13-DOM Manipulation & Style Modification/script.js
@@ -6,3 +6,15 @@ judul.style.color = "blue";
 
 judul.style.fontSize = "50px";
 
+
+// Latihan 
+ judul.textContent = "Handika Saputra";
+
+ deskripsi.innerHTML = "sedang belajar <strong>Full Stack Developer</strong>";
+
+ judul.style.color ="green";
+ judul.style.fontSize = "35px";
+
+ deskripsi.style.color = "grey";
+
+//  Latihan 1 — Profil
