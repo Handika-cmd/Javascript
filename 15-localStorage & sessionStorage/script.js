9ed9e5f0f02c4ab3edb54b9a8c38b07b0b9c3abb
Handika Saputra <handikasaputra484@gmail.com>
2026-09-25 11:02:28 +0700

    localStorage & sessionStorage

--- 15-localStorage & sessionStorage/script.js
+++ 15-localStorage & sessionStorage/script.js
@@ -1,8 +1,32 @@
-localStorage.setItem("tema", "dark");
+// localStorage.setItem("tema", "dark");
 
-const tema = localStorage.getItem("tema");
-console.log(tema);
+// const tema = localStorage.getItem("tema");
+// console.log(tema);
 
-sessionStorage.setItem("nama", "Handika");
-const nama = sessionStorage.getItem("nama");
-console.log(nama);
+// sessionStorage.setItem("nama", "Handika");
+// const nama = sessionStorage.getItem("nama");
+// console.log(nama);
+
+
+// latihan kecil yang menggabungkan Day 14 + Day 15.
+
+
+const nama = document.getElementById("nama");
+const hasil = document.getElementById("hasil");
+
+button.addEventListener("click", function(event){
+
+    event.preventDefault();
+
+    hasil.textContent = "Nama: " + nama.value;
+    localStorage.setItem("nama", nama.value);
+});
+
+button.addEventListener("click", function(reset){
+
+    reset.preventDefault();
+    localStorage.removeItem("nama");
+})
+
+const namaTersimpan = localStorage.getItem("nama");
+console.log(namaTersimpan);
